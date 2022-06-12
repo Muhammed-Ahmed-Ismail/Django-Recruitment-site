8456ea8feda8f69f5f4b75cca837f392ff96ed95
Adham <adhamahmed1234321@gmail.com>
2022-06-12 14:45:22 +0200

    jobs basic crud

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from jobs.models import Job
 # Register your models here.
+admin.site.register(Job)
---
+++ jobs/serializer.py
@@ -0,0 +1,25 @@
+from django.forms import ChoiceField
+from rest_framework import serializers
+from django.db import models
+from jobs.models import Job
+# from django.db.models.fields. import TextField
+
+
+class JobSerializer(serializers.ModelSerializer):
+  
+    class Meta:
+        model=Job
+        fields='__all__'
+
+#         [
+#         'name',
+#         'creation_time',
+#         'Modification_time',
+#         'Description',
+#         # 'tags' ,
+#         # 'applied_developers',
+#         'developer',
+#         'created_by' ,
+#         'status' ,
+        # ]
+
