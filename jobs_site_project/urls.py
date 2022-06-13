1452f20756df1e1f4cf4a873ff4818d47838ab9c
Adham <adhamahmed1234321@gmail.com>
2022-06-13 14:34:46 +0200

    remove cashed file

--- jobs_site_project/urls.py
+++ jobs_site_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jobs/', include('jobs.urls'))
+    path('jobs/', include('jobs.api.v1.urls'))
 ]
